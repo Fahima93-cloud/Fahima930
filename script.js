7e35e633d2251e5bdba31400ed0784a204f5a5b9
Fahima93-cloud <fahima.practice03@gmail.com>
2025-03-22 01:41:43 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    alert("Welcome to ShopEase!");
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
+}
